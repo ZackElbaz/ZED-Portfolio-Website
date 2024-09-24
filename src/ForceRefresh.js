f1a2e079c8e0ef7bdec50935cdd57e7d509baea3
zackel-baz <na19257@bristol.ac.uk>
2024-09-24 21:39:29 +0100

    Newest version

--- src/ForceRefresh.js
+++ src/ForceRefresh.js
@@ -3,18 +3,18 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //// LIVE SITE
 
-// import { useEffect } from 'react';
+import { useEffect } from 'react';
 
-// function ForceRefresh() {
-//   useEffect(() => {
-//     // Directly set the URL to ensure a full page reload
-//     window.location.href = 'http://zackelbaz.github.io/ZED-Portfolio-Website';
-//   }, []);
+function ForceRefresh() {
+  useEffect(() => {
+    // Directly set the URL to ensure a full page reload
+    window.location.href = 'http://zackelbaz.github.io/ZED-Portfolio-Website';
+  }, []);
 
-//   return null; // This component doesn't need to render anything
-// }
+  return null; // This component doesn't need to render anything
+}
 
-// export default ForceRefresh;
+export default ForceRefresh;
 
 
 
@@ -25,18 +25,18 @@
 //// TESTING/LOCALHOST
 
 
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
 
-function ForceRefresh() {
-  const navigate = useNavigate();
+// function ForceRefresh() {
+//   const navigate = useNavigate();
 
-  useEffect(() => {
-    // Force the browser to reload the page from the server
-    navigate('/');
-  }, [navigate]);
+//   useEffect(() => {
+//     // Force the browser to reload the page from the server
+//     navigate('/');
+//   }, [navigate]);
 
-  return null; // This component doesn't need to render anything
-}
+//   return null; // This component doesn't need to render anything
+// }
 
-export default ForceRefresh;
+// export default ForceRefresh;
--- src/projectsData.js
+++ src/projectsData.js
@@ -4,84 +4,84 @@ export const projects = [
     name: 'BicycleAmbulance',
     displayName: 'Bicycle Ambulance',
     tags: ['Medical', 'Emergency', 'University'],
-    date: new Date('December 2020'),
+    date: new Date('2020-12'),
     imagePath: require('./Projects/BicycleAmbulanceImages/ProjectImage.jpg')
     },
     {
     name: 'Boeing737Seat', 
     displayName: 'Boeing 777-300 Seat', 
     tags: ['Aviation', 'University'], 
-    date: new Date('May 2020'),
+    date: new Date('2020-05'),
     imagePath: require('./Projects/Boeing737SeatImages/ExplodedView.png')
     },
     { 
     name: 'ColouredHalftone', 
     displayName: 'Coloured Halftones', 
     tags: ['Art'], 
-    date: new Date('December 2019'),
+    date: new Date('2020-05'),
     imagePath: require('./Projects/ColouredHalftoneImages/halftoneTemp.jpg') 
     },
     { 
     name: 'FireRiskAssessment', 
     displayName: 'Domestic Fire Risk Assessment Robot', 
     tags: ['Medical', 'Safety', 'Robotics', 'University'], 
-    date: new Date('May 2021'),
+    date: new Date('2021-05'),
     imagePath: require('./Projects/BicycleAmbulanceImages/ProjectImage.jpg')
     },
     { 
     name: 'CausticLensGenerator', 
     displayName: 'Caustic Lens Generator', 
     tags: ['Art'], 
-    date: new Date('May 2020'),
+    date: new Date('2020-05'),
     imagePath: require('./Projects/CausticLensGeneratorImages/SteveMould.jpg')
     },
     { 
     name: 'LightbugRTK', 
     displayName: 'Real Time Kinematic GPS Device', 
     tags: ['Device', 'DFM'], 
-    date: new Date('May 2022'),
+    date: new Date('2022-05'),
     imagePath: require('./Projects/LightbugRTKImages/FinalProduct.png')
     },
     { 
     name: 'MechanicalMirror', 
     displayName: 'Mechanical Mirror', 
     tags: ['Art', 'Robotics'], 
-    date: new Date('May 2020'),
+    date: new Date('2020-05'),
     imagePath: require('./Projects/MechanicalMirrorImages/WIRED.jpg') 
     },
     { 
     name: 'PhysicalGIF', 
     displayName: 'Physical GIF Generator', 
     tags: ['Art'], 
-    date: new Date('May 2020'),
+    date: new Date('2020-05'),
     imagePath: require('./Projects/PhysicalGIFImages/Carnovsky.jpg')
     },
     { 
     name: 'RaceTrackerGPS', 
     displayName: 'RaceTracker GPS', 
     tags: ['Device', 'DFM'], 
-    date: new Date('March 2022'),
+    date: new Date('2022-03'),
     imagePath: require('./Projects/RaceTrackerGPSImages/emptytrays.png')
     },
     { 
     name: 'ReactionDiffusion', 
     displayName: 'Reaction-Diffusion Art', 
     tags: ['Art'], 
-    date: new Date('May 2020'),
+    date: new Date('2020-05'),
     imagePath: require('./Projects//ReactionDiffusionImages/Placeholder.jpg')
     },
     { 
     name: 'ThreeDGPS', 
     displayName: '3D GPS Tracker', 
     tags: ['Device', 'Research'], 
-    date: new Date('February 2022'),
+    date: new Date('2022-02'),
     imagePath: require('./Projects/ThreeDGPSImages/Triangulation.png')
     },
     { 
     name: 'TKRTestDevice', 
     displayName: 'Knee Implant Test Rig', 
     tags: ['Robotics', 'Research', 'University'], 
-    date: new Date('May 2024'),
+    date: new Date('2024-05'),
     imagePath: require('./Projects/TKRTestDeviceImages/CloseUp.png')
     },
 ];
